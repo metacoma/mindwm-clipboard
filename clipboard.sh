36a6108965716b17637bab5ef7564788848cf9c5
Sergey Ryabin <ryabin.ss@gmail.com>
2026-01-18 14:50:58 +0400

    init

---
+++ clipboard.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -x
+
+PATH=${PATH}:~/bin
+
+case $1 in
+    clipboard)
+        notify_selection=clipboard
+        xclip_selection=primary
+    ;;
+    qt)
+        notify_selection=clipboard
+        xclip_selection=clipboard
+    ;;
+    primary)
+        notify_selection=primary
+        xclip_selection=primary
+    ;;
+esac
+
+while clipnotify -s ${notify_selection}; do
+    echo "new clipboard"
+    xclip -s ${xclip_selection} -o | tee > /tmp/clipboard.txt
+    tmpdir="$(mktemp -d)"
+    export tmpdir
+    rm input.yaml
+    bash ./handlers.sh ${tmpdir}
+    #yq eval-all '. as $item ireduce ({}; . * $item)' "${tmpdir}"/*.yaml | tee input.yaml
+    cat ${tmpdir}/*.yaml | tee input.yaml
+    if [ -s input.yaml ]; then
+      kcl run ./menu.k --format json > menus.json
+      mv menus.json ~/.config/kando/menus.json
+      kando -m root &
+    fi
+done
---
+++ handlers/ip4.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+ipv4_is_public() {
+  local ip="$1"
+
+  ipcalc -c "$ip" >/dev/null 2>&1 || return 1
+
+  ipcalc -n -b "$ip" \
+    | grep -iqE 'PRIVATE|LOOPBACK|LINKLOCAL|MULTICAST|RESERVED' \
+    && return 1
+
+  return 0
+}
+
+get_country() {
+  local ip_addr=$1
+  mmdblookup --file geoip/GeoLite2-City.mmdb --ip ${ip_addr} country iso_code \
+| sed -n 's/.*"\(.*\)".*/\1/p' | tail -n1
+}
+
+get_org() {
+  local ip_addr=$1
+  mmdblookup \
+    --file geoip/GeoLite2-ASN.mmdb \
+    --ip "$ip_addr" \
+    autonomous_system_organization \
+  | sed -n 's/.*"\(.*\)".*/\1/p' | tail -n1
+}
+
+get_city() {
+  local ip="$1"
+  local lang="${2:-en}"
+
+  mmdblookup \
+    --file geoip/GeoLite2-City.mmdb \
+    --ip "$ip" \
+    city names "$lang" 2>/dev/null \
+  | sed -n 's/.*"\(.*\)".*/\1/p' \
+  | tail -n1
+}
+
+
+tmpdir="${1:?tmpdir is required}"
+IPS=`grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u`
+
+test -z "$IPS" && exit 0
+
+echo "ip4:"
+for ip in $IPS; do
+  echo "  - ip: $ip"
+  country=""
+  ipv4_is_public $ip && {
+    country=$(get_country $ip)
+    test -n "${country}" && \
+      echo "    country: $country"
+    org=$(get_org $ip)
+    test -n "${org}" && \
+      echo "    org: $org"
+    city=$(get_city $ip)
+    test -n "${city}" && \
+      echo "    city: $city"
+
+  } || :
+done
