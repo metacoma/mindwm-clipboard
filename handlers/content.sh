6959152ed027a2b36a37ae18ad954a7d6b29c8ff
Sergey Ryabin <ryabin.ss@gmail.com>
2026-01-28 23:37:06 +0400

    feat(content): normalize file output

--- handlers/content.sh
+++ handlers/content.sh
@@ -1,28 +1,57 @@
 #!/usr/bin/env bash
 
+
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/utils.sh"
+
+file_type_normalize() {
+  awk '
+  {
+    line=$0
+    # remove numbers
+    gsub(/\([^)]+\)/, "", line)
+
+    # remove tails
+    sub(/,? with .*/, "", line)
+
+    # убрать архитектуру / размеры / детали
+    sub(/, [0-9].*/, "", line)
+
+    sub(/JSON text data/, "JSON text", line)
+    sub(/ELF .* executable/, "ELF executable", line)
+    sub(/ image data.*/, " image", line)
+    gsub(/  +/, " ", line)
+    gsub(/, $/, "", line)
+
+    print line
+  }
+  '
+}
+
 
 TMP_FILE=$(mktemp /tmp/content.XXXXX)
 
 cat > ${TMP_FILE}
 
-file_type=$(file -b ${TMP_FILE})
+file_type=$(file -b ${TMP_FILE} | file_type_normalize)
 
 case ${file_type} in
-  "JSON text data")
+  "JSON text")
 cat<<EOF
 json:
   - path: ${TMP_FILE}
 EOF
    ;;
-#   "ASCII text"|"ASCII text, with CRLF line terminators")
-#
-#     cat ${TMP_FILE} | yq > /dev/null 2>&1 && {
-# cat<<EOF
-# yaml:
-#   - path: ${TMP_FILE}
-# EOF
-# }
-#   ;;
+   "ASCII text")
+cat<<EOF
+text:
+  - path: ${TMP_FILE}
+EOF
+   ;;
+   "Unicode text, UTF-8 text")
+cat<<EOF
+text:
+  - path: ${TMP_FILE}
+EOF
+   ;;
 esac
