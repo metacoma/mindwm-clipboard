a18a5e1d19397dcd21597093b3bafcfc814130be
Sergey Ryabin <ryabin.ss@gmail.com>
2026-02-25 13:53:18 +0400

    fix(zabbix): output data only if host found in zabbix

--- handlers/zabbix/zabbix.py
+++ handlers/zabbix/zabbix.py
@@ -5,10 +5,12 @@ import sys
 hostnames = sys.argv[1:]
 
 hosts = get_hosts(hostnames)
-data = {
-    "monitoring": {
-        "hosts": [h.model_dump(exclude_none=True) for h in hosts]
-    }
-}
 
-print(yaml.safe_dump(data, sort_keys=False, allow_unicode=True))
+if len(hosts):
+    data = {
+        "monitoring": {
+            "hosts": [h.model_dump(exclude_none=True) for h in hosts]
+        }
+    }
+
+    print(yaml.safe_dump(data, sort_keys=False, allow_unicode=True))
